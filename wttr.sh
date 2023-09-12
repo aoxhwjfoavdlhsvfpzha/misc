c233c12eb4020e722b0c70731ae1535c16f945d9
aoxhwjfoavdlhsvfpzha <58840974+aoxhwjfoavdlhsvfpzha@users.noreply.github.com>
2023-09-11 22:10:41 -0700

    Add files via upload

--- wttr.sh
+++ wttr.sh
@@ -1,3 +1,4 @@
+#Simple script gets your IP, then zipode, then current weather conditions from various sites
 EXTIP=$(curl -s https://ipinfo.io/ip)
 ZIPCODE=$(curl -s "http://ip-api.com/json/$EXTIP" | grep -oE "zip\"\:\"[0-9]{5}\"" | grep -oE [0-9]{5})
 curl -s "wttr.in/$ZIPCODE?0u"
